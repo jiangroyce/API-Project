85721782daf03c5bcfd5e748163c185b177da5c9
Royce Jiang <jiangroyce@gmail.com>
2023-12-19 16:18:00 -0800

    backend phase2 completed

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,10 @@ const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 const app = express();
+
+// Middlewares
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
@@ -33,6 +36,44 @@ app.use(csurf({
     }
 }));
 
+// Connect routes
 app.use(routes);
+
+// unhandled requests error handler
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource could not be found.");
+    err.title = "Resource Not Found";
+    err.errors = {
+        message: "The requested resource could not be found."
+    };
+    err.status = 404;
+    next(err);
+})
+
+// sequelize errors
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = 'Validation error';
+        err.errors = errors;
+    }
+    next(err);
+});
+
+// error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
 
 module.exports = app;
