fc58b1b2f7d26bdbdb25f82db293317740792b8a
Royce Jiang <jiangroyce@gmail.com>
2023-12-20 15:26:27 -0800

    backend phase3 connected auth middlewares

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth.js");
+router.use(restoreUser);
+const { User } = require("../../db/models");
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
+
+// test user auth middlewares
+router.get("/set-token-cookie", async (_req, res) => {
+  const user = await User.findOne({
+    where: { username: "Demo-lition"}
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user});
+});
+
+// GET /api/restore-user
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
+
+// GET /api/require-auth
+router.get("/require-auth", requireAuth, (req, res) => {
+  return res.json(req.user)
+})
 
 module.exports = router;
 
